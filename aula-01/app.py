14342ae1eb91f1c9fad56b567af3af44ea924398
Julia <juliaalabi@gmail.com>
2025-02-18 11:48:20 -0300

    juliaaa

--- aula-01/app.py
+++ aula-01/app.py
@@ -1,13 +1,29 @@
-#importando o flask
-from flask import Flask
+# importando o flask
+from flask import Flask, render_template
 
-#carregando o flask na variavel app
-app = Flask(__name__)
+# carregando o flask na variavel app
+app = Flask(__name__, template_folder='views')
+
 
 @app.route('/')
-def home ():
-    return '<h1>Bem vindo ao meu primeiro site em flask</h1>'
+def home():
+    return render_template('index.html')
 
-#iniciando o servidor no locaçhost, porta 5000, modo de depuração ativado
+@app.route('/games')
+def games():
+    titulo= 'cs-go'
+    ano= 2012
+    categoria = 'FPS Online'
+    jogadores = ['Julia', 'Yasmin', 'Millie']
+    jogos =['Roblox', 'Minecraft', 'Talking Tom', 'Tetris', 'Super Mario Boss', 'Pacman']
+    return render_template('games.html',
+                           titulo= titulo,
+                           ano=ano,
+                           categoria=categoria,
+                           jogadores=jogadores,
+                           jogos=jogos)
+
+
+# iniciando o servidor no locaçhost, porta 5000, modo de depuração ativado
 if __name__ == '__main__':
- app.run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
